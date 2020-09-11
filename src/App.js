440dd0c07aebf3ad0936d3e273e151c308ec1faa
iandemed <iandemed@gmail.com>
2020-09-11 13:28:08 -0400

    Intialize React App with Icon

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import presIcon from './president-icon-white.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={presIcon}  alt="logo" />
+        <h1>First Families API</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
