5590cd8c709b7c3a5e274a91fab31b93dd8048a5
iandemed <iandemed@gmail.com>
2020-09-11 16:19:31 -0400

    Added button to make a GET request

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import presIcon from './president-icon-white.svg';
 import './App.css';
 
+const firstFamilyURL = 'https://first-families-api.herokuapp.com/'
+
 function App() {
+
+  const [firstFamilies, setFirstFamily] = useState([])
+
+  const getPresidents = () => {
+    fetch(firstFamilyURL)
+    .then(res=> res.json())
+    .then(ffData => {
+      console.log(ffData)
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={presIcon}  alt="logo" />
         <h1>First Families API</h1>
       </header>
+
+      <button onClick={getPresidents}> GET </button>
     </div>
   );
 }
