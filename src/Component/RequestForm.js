9057486b536bd635ad43fed485864161138b930a
iandemed <iandemed@gmail.com>
2020-09-12 17:43:12 -0400

    Begin the construction of the HTTP request

--- src/Component/RequestForm.js
+++ src/Component/RequestForm.js
@@ -1,24 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import '../style/RequestForm.css'
 
 
+
+
 const RequestForm = ({apiURL, onHTTPRequest}) => {
+
+    const [verb, setVerb] = useState('GET')
+
+    const handleVerbSelect = (e) => {
+        setVerb(e.target.value)
+        console.log(e.target.value)
+    }
 
     return (
         <form className="HTTP-request">
-            <select name="verb">
-                <option value="GET"> GET </option>            
-                <option value="POST"> POST </option>            
-                <option value="PUT"> PUT </option>            
-                <option value="DELETE"> DELETE </option>            
-            </select>
-            <p>{apiURL}</p>
-            <select name="resource">
-                <option value="president"> president </option>            
-                <option value="firstSpouse"> firstSpouse </option>                       
-            </select>
-            <p>/</p>
+            <div className="request-line">
+                <select value={verb} onChange={handleVerbSelect}>
+                    <option value="GET"> GET </option>            
+                    <option value="POST"> POST </option>            
+                    <option value="PUT"> PUT </option>            
+                    <option value="DELETE"> DELETE </option>            
+                </select>
+                <p>{apiURL}</p>
+                <select name="resource">
+                    <option value="president"> president </option>            
+                    <option value="firstSpouse"> firstSpouse </option>                       
+                </select>
+                <p>/</p>
+            </div>
+            <div className="submit">
+                <p>SUBMIT</p>
+            </div>
         </form>
     )
 }
