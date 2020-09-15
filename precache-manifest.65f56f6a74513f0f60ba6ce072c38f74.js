b1aab4257dd2b2dca09ec8d4cd5dd6129a975028
iandemed <iandemed@gmail.com>
2020-09-15 15:59:18 -0400

    Updates

---
+++ precache-manifest.65f56f6a74513f0f60ba6ce072c38f74.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08294c9ccae11e231823513c559e17ac",
+    "url": "/first-families/index.html"
+  },
+  {
+    "revision": "4b2f909eca3440b19fad",
+    "url": "/first-families/static/css/main.1b408580.chunk.css"
+  },
+  {
+    "revision": "04b99fe2eca78431d715",
+    "url": "/first-families/static/js/2.841275b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-families/static/js/2.841275b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b2f909eca3440b19fad",
+    "url": "/first-families/static/js/main.8f1f8ed1.chunk.js"
+  },
+  {
+    "revision": "47faffda2727ac3800b2",
+    "url": "/first-families/static/js/runtime-main.93021532.js"
+  },
+  {
+    "revision": "633155027d614452db214ae5e8aa60b6",
+    "url": "/first-families/static/media/github-mark.63315502.svg"
+  },
+  {
+    "revision": "dac9b2cab1259c6ac6e675480cb820d1",
+    "url": "/first-families/static/media/president-icon-white.dac9b2ca.svg"
+  }
+]);
